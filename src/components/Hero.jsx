27dc6e52842563f473bd85376e17ee12594d12a7
unknown <sayakhajra02@gmail.com>
2024-11-06 13:40:55 +0530

    Changes three

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Typewriter from 'typewriter-effect'
-import propilepic from "../assets/Profile.jpeg"
+import propilepic from "../assets/profile.jpeg"
 import CV from '../assets/SayakResume.pdf'
 import { motion } from 'framer-motion'
 const Hero = () => {
